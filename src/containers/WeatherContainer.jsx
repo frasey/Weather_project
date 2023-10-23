f7746b1c24007dc96b4900804f4431ec881023e6
Mikolaj <miksob@gmail.com>
2023-10-23 13:56:27 +0100

    date displayed once per day

--- src/containers/WeatherContainer.jsx
+++ src/containers/WeatherContainer.jsx
@@ -2,7 +2,6 @@ import WeatherForm from "../components/WeatherForm";
 
 const WeatherContainer = ({weather, selectCity, selectDays}) => {
 
-
     let localTime = weather.location.localtime   
 
     let localTimeEpoch = weather.location.localtime_epoch   
@@ -13,30 +12,34 @@ const WeatherContainer = ({weather, selectCity, selectDays}) => {
 
     let cityTemperatureDaily = []
 
-    cityTemperatureDaily = weather.forecast.forecastday.map((day) => (
-        <div key={day.date}>
-            {day.hour
-                .filter((time) => time.time_epoch >= localTimeEpoch - 3600)
-                .map((time) => {
-                    const dateTime = new Date(time.time);
+    cityTemperatureDaily = weather.forecast.forecastday.map((day) => {
+        
+        const dailyDate = new Date(day.date)
 
-                    const formattedDate = dateTime.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' });
+        const formattedDailyDate = dailyDate.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
 
-                    const formattedTime = dateTime.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit', hour12: true })
-                    
-                    const dateFirstLetterUpperCase = formattedDate.charAt(0).toUpperCase() + formattedDate.slice(1)
+        const dateFirstLetterUpperCase = formattedDailyDate.charAt(0).toUpperCase() + formattedDailyDate.slice(1)
 
-                    return (
-                    <div key={time.time_epoch}>
-                        Day: {dateFirstLetterUpperCase} <br/>
-                        Time: {formattedTime} <br/>
-                        Temp: {time.temp_c} °C
-                    </div>
-                    )
-                })
-            }  
-        </div>
-    ));
+        return(
+            <div key={day.date}>
+                <p>{dateFirstLetterUpperCase}</p>
+                {day.hour
+                    .filter((time) => time.time_epoch >= localTimeEpoch - 3600)
+                    .map((time) => {
+                        const dateTime = new Date(time.time);
+
+                        const formattedTime = dateTime.toLocaleTimeString(undefined, { hour: '2-digit', minute: '2-digit', hour12: true })
+                        
+                        return (
+                        <div key={time.time_epoch}>
+                            Time: {formattedTime} <br/>
+                            Temp: {time.temp_c} °C
+                        </div>
+                        )
+                    })
+                }  
+            </div>)
+});
 
     return (  
         <>
