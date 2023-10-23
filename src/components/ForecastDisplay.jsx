8e6e443665f7d0ce2c44f7896e403ba4c9c5856c
Issy Arnaud <yarnaud36@gmail.com>
2023-10-23 16:38:24 +0100

    added changes

--- src/components/ForecastDisplay.jsx
+++ src/components/ForecastDisplay.jsx
@@ -12,21 +12,25 @@ const ForecastDisplay = ({weather}) => {
 
     let cityTemperatureDaily = []
 
-    cityTemperatureDaily = weather.forecast.forecastday.map((day) => (
-        <div key={day.date}>
-            {day.hour
+    cityTemperatureDaily = weather.forecast.forecastday.map((day) => {
+        const dailyDate = new Date(day.date)
+        const formattedDailyDate = dailyDate.toLocaleDateString(undefined, { weekday: 'long', year: 'numeric', month: 'long', day: 'numeric' })
+        const dateFirstLetterUpperCase = formattedDailyDate.charAt(0).toUpperCase() + formattedDailyDate.slice(1)
+        return (
+            <div key={day.date}>
+                <p>{dateFirstLetterUpperCase}</p>
+                {day.hour
                 .filter((time) => time.time_epoch >= localTimeEpoch - 3600)
                 .map((time) => {
-
                     return (
                         <DailyDisplay key={time.time_epoch} time={time}/>
                     )
                 })
-            }  
-        </div>
-    ));
-
-    return (  
+            }
+            </div>
+        )
+    })
+        return (  
         <>
             <p>City: {cityName}</p>
             <p>Local time: {localTime}</p>
